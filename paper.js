0650e80698e945c15e036ac8580cb5e939d5da08
SinghArya <75117996+SinghArya@users.noreply.github.com>
2021-05-28 18:01:32 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,30 @@
+class Paper{
+    constructor(x,y,r){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2,
+
+        }
+        this.image=loadImage("paper.png");
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.body=Bodies.circle(this.x,this.y,(this.r-20)/2,options);
+        
+        World.add(world,this.body);
+
+    }
+    display(){
+        var position=this.body.position;
+        push();
+        translate(position.x,position.y);
+        rectMode(CENTER);
+        imageMode(CENTER);
+        image(this.image,0,0,this.r,this.r);
+        pop();
+    }
+
+
+}
